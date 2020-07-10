d6377786699299651003ceddf75a194e0ab51732
Edson de Morais <edson.morais@outlook.com>
2020-07-10 15:31:14 -0300

    Site MVC Acãochego

---
+++ AcaochegoMVC/Controllers/MateriasController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcaochegoMVC.Controllers
+{
+    public class MateriasController : Controller
+    {
+        // GET: Materias
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChegandoEmCasa()
+        {
+            return View();
+        }
+
+        public ActionResult EstouPreparado()
+        {
+            return View();
+        }
+
+        public ActionResult PoliticaDeAdocao()
+        {
+            return View();
+        }
+
+        public ActionResult PossoDoar()
+        {
+            return View();
+        }
+
+        public ActionResult QueroFazerDoacao()
+        {
+            return View();
+        }
+
+        public ActionResult QueroSerVoluntario()
+        {
+            return View();
+        }
+
+        public ActionResult DenunciaMausTratos()
+        {
+            return View();
+        }
+
+        public ActionResult UmOuDoisCaes()
+        {
+            return View();
+        }
+
+        public ActionResult CaesEGatos()
+        {
+            return View();
+        }
+
+        public ActionResult Castracao()
+        {
+            return View();
+        }
+
+        public ActionResult LaVemOBebe()
+        {
+            return View();
+        }
+
+        public ActionResult PerguntasFrequentes()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AcaochegoMVC/Controllers/PrecisoDeAjudaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcaochegoMVC.Controllers
+{
+    public class PrecisoDeAjudaController : Controller
+    {
+        // GET: PrecisoDeAjuda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
